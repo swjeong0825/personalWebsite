6e91897de463c626111f81ac4176d2eda8dcd16f
Sunwoo <swjeong0825@gmail.com>
2021-07-11 00:05:39 -0400

    remove components/aboutPageComponents and create reusable/aboutPageContentsFormat

--- src/containers/about/about.js
+++ src/containers/about/about.js
@@ -1,16 +1,35 @@
-import Experiences from "../../components/AboutPageComponents/Experiences/experiences";
-import Portfolio from "../../components/AboutPageComponents/Portfolio/portfolio";
-import Hobby from "../../components/AboutPageComponents/Hobby/hobby";
+import UMassAmherstCicsLogo from "../../assets/image/UMassAmherstCICS.png";
+import ACEALogo from "../../assets/image/ACEA-Logo-Web.png";
+import HackUConn2018Logo from "../../assets/image/HackUConn2018Logo.png";
+import HackUMass2020Logo from "../../assets/image/HackUMass2020Logo.png";
+import QHacks2021Logo from "../../assets/image/QHacks2021Logo.png";
+
+import AboutPageContentsFormat from "../../resuableComponents/aboutPageContentsFormat/aboutPageContentsFormat";
+
+// props.contentImagesAndUrlPair: Object {key: String(each subhead), value: [[imported image, String(url)]]}
 
 const About = () => {
   return (
-    <div className='container'>
-      <div className='row mb-3'>
-        <h1>About Me</h1>
-      </div>
-      <Experiences />
-      <Portfolio />
-      <Hobby />
+    <div>
+      <AboutPageContentsFormat
+        head='Education'
+        subheads={["Undergraduate"]}
+        contentImagesAndUrlPairs={{
+          Undergraduate: [UMassAmherstCicsLogo, "https://www.cics.umass.edu/"],
+        }}
+      />
+      <AboutPageContentsFormat
+        head='Experiences'
+        subheads={["Internships", "Hackathons"]}
+        contentImagesAndUrlPairs={{
+          Internships: [ACEALogo, "https://aceaglobal.com/"],
+          Hackathons: [
+            [HackUConn2018Logo, undefined],
+            [HackUMass2020Logo, "https://hackumass.com/"],
+            [QHacks2021Logo, "https://qhacks.io/"],
+          ],
+        }}
+      />
     </div>
   );
 };
